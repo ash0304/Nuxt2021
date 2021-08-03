9059f22c0d4276970d6f0f6fb4a562cbf6feec1f
Ash Lai <z0975201508@gmail.com>
2021-08-03 16:55:19 +0800

    course 16. Validating Parameters

--- my-first-nuxt-app/pages/users/_id/index.vue
+++ my-first-nuxt-app/pages/users/_id/index.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>A single User, with ID: {{ $route.params.id }}</h1>
 </template>
+<script>
+export default {
+  validate(data) {
+    return /^\d+$/.test(data.params.id);
+  }
+};
+</script>
