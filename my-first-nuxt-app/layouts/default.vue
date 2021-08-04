6f222f5a7033dad6a21afe1d09e50311d2934121
Ash Lai <z0975201508@gmail.com>
2021-08-04 16:08:14 +0800

    course 21. Working with 'Normal' Components

--- my-first-nuxt-app/layouts/default.vue
+++ my-first-nuxt-app/layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
-<style>
+<script>
+import Header from "~/components/Header.vue";
 
-</style>
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style></style>
