d0594e111cad01d6c65475bde3021c61b44655e0
Ash Lai <z0975201508@gmail.com>
2021-08-05 13:17:43 +0800

    courese 29. Splitting the Page into Components

--- prj-pages-views/pages/index.vue
+++ prj-pages-views/pages/index.vue
@@ -4,36 +4,36 @@
       <h1>Get the lastest tech new!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="`/posts/${1}`" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url(https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg)"
-          ></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="`/posts/${2}`" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url(https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg)"
-          ></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+        title="Hello there!"
+        previewText="This is my first post!"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+        title="Hello there!"
+        previewText="This is my second post!"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+        title="Hello there!"
+        previewText="This is my third post!"
+      />
     </section>
   </div>
 </template>
 
 <script>
-export default {};
+import PostPreview from "~/components/Posts/PostPreview.vue";
+
+export default {
+  components: {
+    PostPreview
+  }
+};
 </script>
 <style scoped>
 .intro {
@@ -73,41 +73,5 @@ export default {};
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
