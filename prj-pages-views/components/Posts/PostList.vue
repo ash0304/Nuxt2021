a2d7cfbccede3b38623da73fb3406e48d774fa44
Ash Lai <z0975201508@gmail.com>
2021-08-06 22:02:18 +0800

    course 33. Working on the Post Page

---
+++ prj-pages-views/components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+      title="Hello there!"
+      previewText="This is my second post!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
+      title="Hello there!"
+      previewText="This is my third post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "~/components/Posts/PostPreview.vue";
+
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- prj-pages-views/pages/index.vue
+++ prj-pages-views/pages/index.vue
@@ -3,35 +3,16 @@
     <section class="intro">
       <h1>Get the lastest tech new!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
-        title="Hello there!"
-        previewText="This is my first post!"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
-        title="Hello there!"
-        previewText="This is my second post!"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="https://www.hydrocarbons-technology.com/wp-content/uploads/sites/9/2020/09/shutterstock_1152185600-1440x1008-1-857x600.jpg"
-        title="Hello there!"
-        previewText="This is my third post!"
-      />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from "~/components/Posts/PostPreview.vue";
+import PostList from "~/components/Posts/PostList.vue";
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 };
 </script>
@@ -65,14 +46,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
